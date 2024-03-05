76a801eda9a9a10b20126eef5a0e755e61e31e85
Alessandro9827 <ale.moramarco27@gmail.com>
2024-03-05 17:42:48 +0100

    change name to model e controller

--- app/Http/Controllers/ApartamentController.php
+++ app/Http/Controllers/ApartamentController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AppartamentController extends Controller
+class ApartamentController extends Controller
 {
     /**
      * Display a listing of the resource.
