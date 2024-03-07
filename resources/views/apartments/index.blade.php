74f13a55eb65e61ddf6372075a8b827c63501ed5
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-03-07 19:34:49 +0100

    modified link for the show of every apartments

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -8,29 +8,29 @@
                 My apartment
             </h1>
             @foreach ($apartments as $apartment)
-            <a href="{{ route('user.apartments.show', $apartment) }}" class="text-decoration-none">
-                
-                <div class="col-3">
-                    <div class="card">
-                        <h3>
-                            {{$apartment->title}}
-                        </h3>
-                        {{-- @dump(json_decode($apartment->imgs)) --}}
-                       {{--  @foreach (json_decode($apartment->imgs) as $img)
-                        <img src="{{$img}}" alt="">
+            
+            <div class="col-3">
+                <div class="card">
+                        <a href="{{ route('user.apartments.show', $apartment) }}" class="text-decoration-none">
+                            <h3>
+                                {{$apartment->title}}
+                            </h3>
+                            {{-- @dump(json_decode($apartment->imgs)) --}}
+                            {{--  @foreach (json_decode($apartment->imgs) as $img)
+                            <img src="{{$img}}" alt="">
+                                
+                            @endforeach --}}
+                            @if (str_starts_with($apartment->img, 'http'))
+                                <img src="{{$apartment->img}}" alt="" >
+                                    
+                            @else
+                                    
+                                <img src="{{ asset ('storage') . '/' . $apartment->img}}" alt="">
                             
-                        @endforeach --}}
-                        @if (str_starts_with($apartment->img, 'http'))
-                            <img src="{{$apartment->img}}" alt="" >
-                                
-                        @else
-                                
-                            <img src="{{ asset ('storage') . '/' . $apartment->img}}" alt="">
-                        
-                        @endif 
+                            @endif 
+                        </a> 
                     </div>
                 </div>
-            </a> 
             @endforeach
         </div>
     </div>
