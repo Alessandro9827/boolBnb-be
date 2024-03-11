4f35a3406c1c78c61a097675d19b8fe289da251b
MatTeo-BT <aboutmet@gmail.com>
2024-03-11 12:58:54 +0100

    modifica rotte api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Guest\ApartmentController;
+use App\Models\Apartment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::name('api.')->group(function () {
+    Route::get('/guest/apartments', [ApartmentController::class, 'index'])->name('guest.apartments.index');
+    Route::get('/guest/apartments/{guest}', [ApartmentController::class, 'show'])->name('guest.apartments.show');
 });
