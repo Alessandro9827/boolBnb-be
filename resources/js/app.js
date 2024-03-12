989d203d5d8c323195268dfa8fe53342c2f1a793
gabrieleomegna <gabrieleomegna01@gmail.com>
2024-03-13 00:24:47 +0100

    changed name of the api search and substitute forEach with for cycle

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,7 +11,7 @@ addressInput.addEventListener ('input', function() {
     const inputValue = this.value.replace(" ", "+");
     ulElement.innerHTML = '';
     const apiKey='9B3Txp0d4DICteHUwWohHtaZOMm3WCUY';
-    let coordinate = `https://api.tomtom.com/search/2/geocode/${inputValue}.json?key=${apiKey}`;
+    let coordinate = `https://api.tomtom.com/search/2/search/${inputValue}.json?key=${apiKey}`;
         
     async function file_get_contents(uri, callback) {
         let res = await fetch(uri),
@@ -22,19 +22,33 @@ addressInput.addEventListener ('input', function() {
     file_get_contents(coordinate).then((response) => {
         result = JSON.parse(response)
         // console.log(result)
-        result.results.forEach(element => {
-            // console.log(element.address);
+        for (let i = 0; i < 4; i++) {
             const li = document.createElement('li');
+            li.textContent = result.results[i].address.freeformAddress;
             li.classList.add('resultItem');
-            li.textContent = element.address.freeformAddress;
             li.addEventListener('click',() => {
-                this.value =  element.address.freeformAddress;
+                this.value =  result.results[i].address.freeformAddress;
+                console.log(this)
                 
                 ulElement = document.getElementById('searchResults');
                 ulElement.innerHTML = '';
                 ulElement.classList.add('d-none')
             })
             ulElement.appendChild(li);
-        });
+        }
+        // result.results.forEach(element => {
+        //     // console.log(element.address);
+        //     const li = document.createElement('li');
+        //     li.classList.add('resultItem');
+        //     li.textContent = element.address.freeformAddress;
+        //     li.addEventListener('click',() => {
+        //         this.value =  element.address.freeformAddress;
+                
+        //         ulElement = document.getElementById('searchResults');
+        //         ulElement.innerHTML = '';
+        //         ulElement.classList.add('d-none')
+        //     })
+        //     ulElement.appendChild(li);
+        // });
     });
 })
